7f467aa1819baabaae2e7226b65d61db51823166
Petter79 <45226725+Petter79@users.noreply.github.com>
2023-12-19 13:49:18 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,142 +1,131 @@
-// Compile with: g++ ./main.cpp -o main -lSDL2 -lSDL2_image
-
-#include <stdlib.h>
+#include <cstdlib>
+#include <ctime>
+#include <cstring>
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_image.h>
-#include <time.h>
-#include <stdio.h>
 
-///////////////////////////////////////////////////////////////////////////////
-// Global variables
-///////////////////////////////////////////////////////////////////////////////
-int game_is_running = true; // In summary, game_is_running is a control variable that determines whether the game loop should continue running or if it's time to exit the game.
-SDL_Window* window = NULL;
-SDL_Renderer* renderer = NULL;
-SDL_Texture* texture = NULL;
-SDL_Texture* background = NULL;
-SDL_Color color = {255, 255, 255};
-int WINDOW_WIDTH = 1920;
-int WINDOW_HEIGHT = 1080;
+// Control variable to determine whether the game loop should continue running
+bool isGameRunning = true;
 
-///////////////////////////////////////////////////////////////////////////////
-// Function to get current date and time
-///////////////////////////////////////////////////////////////////////////////
-char* datetime() {
-    char* array = (char*)malloc(sizeof(char) * 25);
-    time_t result;
-    result = time(NULL);
-    sprintf(array, "%s", asctime(localtime(&result)));
-    array[25] = '\0';
-    return array;
-}
+// SDL-related variables
+SDL_Window* mainWindow = nullptr;
+SDL_Renderer* mainRenderer = nullptr;
+SDL_Texture* characterTexture = nullptr;
+SDL_Texture* backgroundTexture = nullptr;
+SDL_Color textColor = {255, 255, 255};
+const int WindowWidth = 1920;
+const int WindowHeight = 1080;
 
-///////////////////////////////////////////////////////////////////////////////
-// Function to poll SDL events and process keyboard input
-///////////////////////////////////////////////////////////////////////////////
-void process_input(void) {
+// Function to handle SDL events and process keyboard input
+void handleEvents(void) {
     SDL_Event event;
     while (SDL_PollEvent(&event)) {
         switch (event.type) {
             case SDL_QUIT:
-                game_is_running = false; // Quit the game loop if the window is closed
+                isGameRunning = false; // Quit the game loop if the window is closed
                 break;
             case SDL_KEYDOWN:
                 if (event.key.keysym.sym == SDLK_ESCAPE) {
-                    game_is_running = false; // Quit the game loop if the Escape key is pressed
+                    isGameRunning = false; // Quit the game loop if the Escape key is pressed
                 }
                 break;
         }
     }
 }
 
-///////////////////////////////////////////////////////////////////////////////
-// Render number to screen
-///////////////////////////////////////////////////////////////////////////////
-void renderText(int col, int x, int y, int scale) {
-    SDL_Rect texturePart;
-    SDL_Rect textureRect;
+// Render a character sprite on the screen
+void renderCharacterSprite(int characterIndex, int screenX, int screenY, int scale) {
+    SDL_Rect spriteSourceRect;  // Portion of the character texture to render
+    SDL_Rect destinationRect;   // Position and size on the screen
 
-    int row = 0;
+    int spriteRow = 0;
 
-    if (col > 15) {
-        while (col > 15) {
-            col -= 16;
-            row++;
+    // Adjust column and row indices for character spritesheet
+    if (characterIndex > 15) {
+        while (characterIndex > 15) {
+            characterIndex -= 16;
+            spriteRow++;
         }
     }
 
-    texturePart = {col * 36 + 38, row * 48 + 50, 26, 40}; // Define the portion of the texture to render
-    textureRect = {x * scale, y, 26 * scale, 40 * scale}; // Define the position and size on the screen
-    SDL_RenderCopy(renderer, texture, &texturePart, &textureRect); // Copy the texture to the renderer
+    // Define the portion of the texture to render
+    spriteSourceRect = {characterIndex * 36 + 38, spriteRow * 48 + 50, 26, 40};
+    
+    // Define the position and size on the screen
+    destinationRect = {screenX * scale, screenY, 26 * scale, 40 * scale};
+    
+    // Copy the character texture to the renderer
+    SDL_RenderCopy(mainRenderer, characterTexture, &spriteSourceRect, &destinationRect);
 }
 
-void printString(const char* text, int l, int x, int y, int scale) {
-    for (int i = 0; i < l; i++) {
-        renderText(text[i], x + i * 26, y, scale); // Render each character in the string
+// Render a string to the screen
+void renderString(const char* text, int length, int x, int y, int scale) {
+    for (int i = 0; i < length; i++) {
+        renderCharacterSprite(text[i], x + i * 26, y, scale); // Render each character in the string
     }
 }
 
-///////////////////////////////////////////////////////////////////////////////
 // Render function to draw game objects in the SDL window
-///////////////////////////////////////////////////////////////////////////////
 void render(void) {
-    SDL_RenderClear(renderer); // Clear the renderer
-    SDL_RenderCopy(renderer, background, NULL, NULL); // Render the background texture
+    // Clear the renderer
+    SDL_RenderClear(mainRenderer);
 
-    time_t rawtime;
-    struct tm* timeinfo;
+    // Render the background texture
+    SDL_RenderCopy(mainRenderer, backgroundTexture, nullptr, nullptr);
+
+    // Get current time
+    time_t rawTime;
+    struct tm* timeInfo;
 
     char buffer[25];
-    time(&rawtime);
-    timeinfo = localtime(&rawtime);
+    time(&rawTime);
+    timeInfo = localtime(&rawTime);
 
-    snprintf(buffer, sizeof(buffer), "%d-%d-%d %02d:%02d:%02d", timeinfo->tm_mday, timeinfo->tm_mon + 1,
-             timeinfo->tm_year + 1900, timeinfo->tm_hour, timeinfo->tm_min, timeinfo->tm_sec);
-    printString(buffer, strlen(buffer), 250, 1080 - 108, 2); // Render the formatted date and time
-    SDL_RenderPresent(renderer); // Update the screen
+    // Format the time as a string
+    snprintf(buffer, sizeof(buffer), "%d-%d-%d %02d:%02d:%02d",
+             timeInfo->tm_mday, timeInfo->tm_mon + 1, timeInfo->tm_year + 1900,
+             timeInfo->tm_hour, timeInfo->tm_min, timeInfo->tm_sec);
+
+    // Render the formatted date and time
+    renderString(buffer, strlen(buffer), 250, WindowHeight - 108, 2);
+
+    // Update the screen
+    SDL_RenderPresent(mainRenderer);
 }
 
-///////////////////////////////////////////////////////////////////////////////
 // Update function with a fixed time step
-///////////////////////////////////////////////////////////////////////////////
 void update(void) {
     // Add any update logic here
 }
 
-///////////////////////////////////////////////////////////////////////////////
 // Initializing
-///////////////////////////////////////////////////////////////////////////////
-void init() {
+void initialize() {
     SDL_Init(SDL_INIT_TIMER | SDL_INIT_VIDEO); // Initialize SDL
-    SDL_CreateWindowAndRenderer(WINDOW_WIDTH, WINDOW_HEIGHT, SDL_WINDOW_FULLSCREEN, &window, &renderer); // Create window and renderer
+    SDL_CreateWindowAndRenderer(WindowWidth, WindowHeight, SDL_WINDOW_FULLSCREEN, &mainWindow, &mainRenderer); // Create window and renderer
     IMG_Init(IMG_INIT_PNG); // Initialize SDL_image
-    texture = IMG_LoadTexture(renderer, "Amiga_Charset_16x16.png"); // Load character texture
-    background = IMG_LoadTexture(renderer, "background.jpeg"); // Load background texture
+    characterTexture = IMG_LoadTexture(mainRenderer, "Amiga_Charset_16x16.png"); // Load character texture
+    backgroundTexture = IMG_LoadTexture(mainRenderer, "background.jpeg"); // Load background texture
 }
 
-///////////////////////////////////////////////////////////////////////////////
 // Cleaning up
-///////////////////////////////////////////////////////////////////////////////
-void quit() {
-    SDL_DestroyTexture(texture); // Free character texture
-    SDL_DestroyTexture(background); // Free background texture
+void cleanup() {
+    SDL_DestroyTexture(characterTexture); // Free character texture
+    SDL_DestroyTexture(backgroundTexture); // Free background texture
     IMG_Quit(); // Quit SDL_image
-    SDL_DestroyRenderer(renderer); // Destroy renderer
-    SDL_DestroyWindow(window); // Destroy window
+    SDL_DestroyRenderer(mainRenderer); // Destroy renderer
+    SDL_DestroyWindow(mainWindow); // Destroy window
     SDL_Quit(); // Quit SDL
 }
 
-///////////////////////////////////////////////////////////////////////////////
 // Main function
-///////////////////////////////////////////////////////////////////////////////
 int main(void) {
-    init(); // Initialize the game
-    while (game_is_running) {
-        process_input(); // Process user input
+    initialize(); // Initialize the game
+    while (isGameRunning) {
+        handleEvents(); // Process user input
         update(); // Update game state
         render(); // Render the game
         SDL_Delay(100); // Add a delay to control the frame rate
     }
-    quit(); // Clean up resources
+    cleanup(); // Clean up resources
     return EXIT_SUCCESS; // Exit the program
 }
